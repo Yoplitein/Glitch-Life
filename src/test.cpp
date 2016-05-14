14517a58d951ff661e473e8705bddb8e0da7f2a5
Steven Dwy <yoplitein+git@gmail.com>
2016-05-14 16:46:15 -0700

    Test all path searching functions

--- src/test.cpp
+++ src/test.cpp
@@ -4,17 +4,66 @@
 
 #include "Util.h"
 
+void testRecurseSubdirs();
+void testGetAllSubdirs();
+void testGetSubdirs();
+void testGetDirFiles();
+
 int main(int argc, char **argv)
+{
+    testRecurseSubdirs();
+    testGetAllSubdirs();
+    testGetSubdirs();
+    testGetDirFiles();
+    
+    return 0;
+}
+
+void testRecurseSubdirs()
 {
     std::vector<std::string> dirs;
     
     recurseSubdirs(getWorkDir(), dirs);
-    std::cout << "dirs:\n";
+    std::cout << "recurseSubdirs:\n";
     
     for(string dir: dirs)
         std::cout << "    " << dir << "\n";
     
     std::cout << "===\n";
+}
+
+void testGetAllSubdirs()
+{
+    auto dirs = getAllSubdirs(getWorkDir());
     
-    return 0;
+    std::cout << "getAllSubdirs:\n";
+    
+    for(string dir: dirs)
+        std::cout << "    " << dir << "\n";
+    
+    std::cout << "===\n";
+}
+
+void testGetSubdirs()
+{
+    auto dirs = getSubdirs(getWorkDir());
+    
+    std::cout << "getSubdirs:\n";
+    
+    for(string dir: dirs)
+        std::cout << "    " << dir << "\n";
+    
+    std::cout << "===\n";
+}
+
+void testGetDirFiles()
+{
+    auto files = getDirFiles(getWorkDir() + std::string("/src/"), std::string("cpp"));
+    
+    std::cout << "getDirFiles:\n";
+    
+    for(string file: files)
+        std::cout << "    " << file << "\n";
+    
+    std::cout << "===\n";
 }
